b69c262b9061abb4fbe68f4fec766da353a058d0
Victoria Fakunle <v.fakunle@alustudent.com>
2024-09-12 14:30:25 +0200

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
-
 def minOperations(n):
+    """
+    Calculates the minimum number of operations required to reach a string of length n.
+
+    The operations allowed are:
+    - Append a character to the end of the string (cost: 1 operation)
+    - Copy the entire string and append it to the end (cost: 1 operation)
+
+    Args:
+        n (int): The length of the target string.
+
+    Returns:
+        int: The minimum number of operations required to reach a string of length n.
+
+    """
     operations = 0
     current_chars = 1
     while current_chars < n:
