8c0cfd94a07c577ad956da65c6e5476a003c268c
root <v.fakunle@alustudent.com>
2024-09-28 04:59:00 -0700

    rain

---
+++ rain/0-rain.py
@@ -0,0 +1,29 @@
+def rain(walls):
+    """
+    Calculate the amount of rainwater retained between walls.
+
+    Args:
+        walls (list): A list of non-negative integers representing the heights of walls.
+
+    Returns:
+        int: The total amount of rainwater retained.
+    """
+    if not walls:
+        return 0
+
+    total_rain = 0
+    left_max = [0] * len(walls)
+    right_max = [0] * len(walls)
+
+    left_max[0] = walls[0]
+    for i in range(1, len(walls)):
+        left_max[i] = max(left_max[i-1], walls[i-1])
+
+    right_max[-1] = walls[-1]
+    for i in range(len(walls)-2, -1, -1):
+        right_max[i] = max(right_max[i+1], walls[i+1])
+
+    for i in range(len(walls)):
+        total_rain += max(0, min(left_max[i], right_max[i]) - walls[i])
+
+    return total_rain
